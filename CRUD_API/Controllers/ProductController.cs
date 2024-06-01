264ceb5db62dab3f4f1b859f570a650ebcca52c2
ZyrM <tduck.contact@gmail.com>
2024-06-01 15:01:54 +0700

    Add project files.

---
+++ CRUD_API/Controllers/ProductController.cs
@@ -0,0 +1,52 @@
+﻿using CRUD_API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD_API.Controllers
+{
+    [Route("product")]
+    public class ProductController : ControllerBase
+    {
+        AppDbContext _dbContext = new AppDbContext();
+        [HttpGet("Get-all-product")]
+        public ActionResult GetAll()
+        {
+            return Ok(_dbContext.Products.ToList());
+        }
+        [HttpGet("Get-by-id")]
+        public ActionResult GetAll(int id)
+        {
+            return Ok(_dbContext.Products.Find(id));
+        }
+        [HttpPost("create-product")]
+        public ActionResult CreateProduct(Product product)
+        {
+            try
+            {
+                _dbContext.Products.Add(product);
+                _dbContext.SaveChanges();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpPut("update-product")]
+        public ActionResult UpdateProduct(Product product)
+        {
+            try
+            {
+                var updateItem = _dbContext.Products.Find(product.Id);
+                updateItem.Name = product.Name;
+                updateItem.Description = product.Description;
+                _dbContext.Products.Update(updateItem);
+                _dbContext.SaveChanges();
+                return Ok();
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ CRUD_API/Models/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_API.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() { }
+        public AppDbContext(DbContextOptions options) : base(options) { }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=LAPTOP-CI9J4P6P;Initial Catalog=API_CRUD;Integrated Security=True;TrustServerCertificate=True");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
